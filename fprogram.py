91d180054e44195c4810999aa8ae62ae7b8ccab3
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2021-03-27 11:50:26 +0530

    added calc

--- fprogram.py
+++ fprogram.py
@@ -3,5 +3,11 @@ def add(a,b):
 	print("Your additon is",a+b)
 def sub(a,b):
 	print("Your Subtraction is ",a-b)
+def mul(a,b):
+	print("Your Multiplication is ",a*b)
+def div(a,b):
+	print("You division is ",a/b)
 add(2,4)
 sub(10,5)
+mul(2,4)
+div(20,2)
